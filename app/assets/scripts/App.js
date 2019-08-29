8ec4efbde48612b78aa415c630e8786fd086f67e
Deltathiago98 <deltathiago98@gmail.com>
2019-08-28 23:36:28 -0400

    Not a real commit

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,12 @@
+var $ = require('jquery');
 var Person = require('./modules/Person');
 
+alert("This lalalalalala")
 
 var john = new Person("john Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
