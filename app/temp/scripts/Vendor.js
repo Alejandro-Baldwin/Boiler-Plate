c25b43e925d412b778fdf78ad3ed0fbd000b4258
Deltathiago98 <deltathiago98@gmail.com>
2019-08-31 19:15:53 -0400

    Completed Lazyloading of images

--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -60,21 +60,22 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */,
-/* 1 */
+/******/ ({
+
+/***/ 8:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(2);
+__webpack_require__(9);
 
 /***/ }),
-/* 2 */
+
+/***/ 9:
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -777,4 +778,5 @@ __webpack_require__(2);
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
